26db05b16b75f42948111c6ea7f1b10eef58eab6
Ankur Kunal <ankur@Ankurs-MacBook-Pro.local>
2023-10-10 11:49:51 +0530

    Add home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Home.css"
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [countdown, setCountdown] = useState(5);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (countdown === 1) {
+        navigate('/store');
+      } else {
+        setCountdown(countdown - 1);
+      }
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [countdown, navigate]);
   return (
-    <div>
-      Home
+    <div className='home-page'>
+      <h1>Home Page</h1>
+      <h3>Please explore all other pages. start with store</h3>
+      <p>Redirecting to /store in</p>
+      <span>{countdown}s...</span>
     </div>
   )
 }
